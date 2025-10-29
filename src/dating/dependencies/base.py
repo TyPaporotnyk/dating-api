947427d122c9dfaa13abb53187a869adc5340b8b
Daniil Svadkovsky <daniil.svadkovsky04@gmail.com>
2025-10-29 20:05:22 +0200

    Chnaged storages location from photos to main directory and added redis

--- src/dating/dependencies/base.py
+++ src/dating/dependencies/base.py
@@ -1,9 +1,11 @@
 from collections.abc import AsyncGenerator
 
 from dishka import Provider, Scope, provide
+from redis.asyncio import Redis, from_url
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from dating.config import (
+    REDIS_URL,
     S3_ACCESS_KEY_ID,
     S3_BUCKET_NAME,
     S3_ENDPOINT_URL,
@@ -13,8 +15,8 @@ from dating.config import (
 from dating.database.core import async_session_maker
 from dating.database.managers.base import TransactionManager
 from dating.database.managers.sqlalchemy import SQLAlchemyTransactionManager
-from dating.photos.storages.base import Storage
-from dating.photos.storages.s3 import S3Credentials, S3Storage
+from dating.storages.base import Storage
+from dating.storages.s3 import S3Credentials, S3Storage
 
 
 class BaseAppProvider(Provider):
@@ -38,3 +40,7 @@ class BaseAppProvider(Provider):
                 secret_access_key=S3_SECRET_ACCESS_KEY,
             )
         )
+
+    @provide(scope=Scope.APP)
+    def get_redis_client(self) -> Redis:
+        return from_url(REDIS_URL)
