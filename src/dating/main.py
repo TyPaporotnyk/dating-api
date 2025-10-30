2a613b5c5a52cb003c7402b205abd4016d6c152b
Daniil Svadkovsky <daniil.svadkovsky04@gmail.com>
2025-10-30 18:22:08 +0200

    Chnaged slow api middleware

--- src/dating/main.py
+++ src/dating/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-from slowapi.middleware import SlowAPIMiddleware
+from slowapi.middleware import SlowAPIASGIMiddleware
 from slowapi.util import get_remote_address
 
 from dating import api
@@ -47,7 +47,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # ty
 
 setup_dishka(container=container, app=app)
 
-app.add_middleware(SlowAPIMiddleware)
+app.add_middleware(SlowAPIASGIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
